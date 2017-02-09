f79cd0ddf58d1c48b9503f5dbb0401e7ed81ea47
InAbsencia <absentia@protonmail.ch>
2017-02-09 19:34:41 +0100

    Added CDIO-D1 template, plus a new extra classes.

---
+++ CDIO-D1/src/dal/IUserDAO.java
@@ -0,0 +1,27 @@
+package dal;
+
+import java.util.List;
+
+import dto.UserDTO;
+
+public interface IUserDAO {
+
+	UserDTO getUser(int userId) throws DALException;
+	List<UserDTO> getUserList() throws DALException;
+	void createUser(UserDTO user) throws DALException;
+	void updateUser(UserDTO user) throws DALException;
+	void deleteUser(int userId) throws DALException;
+
+	public class DALException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public DALException(String msg, Throwable e) {
+			super(msg, e);
+		}
+
+		public DALException(String msg) {
+			super(msg);
+		}
+	}
+
+}
