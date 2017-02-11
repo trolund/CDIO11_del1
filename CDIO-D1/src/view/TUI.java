18cd2276e1f7a908528633a37c2603d15e073abf
troelslund <troelslund@192.168.1.46>
2017-02-11 13:21:43 +0100

    Begyndt på opret bruger metode

--- CDIO-D1/src/view/TUI.java
+++ CDIO-D1/src/view/TUI.java
@@ -1,5 +1,34 @@
 package view;
 
+import java.util.Scanner;
+
 public class TUI implements ITUI {
+	
+	Scanner input = new Scanner(System.in);
+	
+	public void createUser(){ // begyndt på opret bruger... 
+		System.out.println("Ny bruger:");
+		System.out.println("hvad skal bruger id være på brugeren? (vælg mellem 11-99)");
+		
+		boolean tjek = false;
+		int id;
+		
+		while(!tjek){
+			id = input.nextInt();
+			if(id < 11 || id > 99) {
+			tjek = false; 
+			System.err.println("Din id er ikke mellem 11 og 99. prøv igen.");
+			
+			}
+			else tjek = true;
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
