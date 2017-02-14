270f3b693c3986458404f51472d975a276b6c80d
troelslund <troelslund@10.16.143.179>
2017-02-14 19:22:37 +0100

    prøvet at skrive noget til en fil...

--- CDIO-D1/src/data/PData.java
+++ CDIO-D1/src/data/PData.java
@@ -1,12 +1,43 @@
 package data;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class PData implements IData {
+	
+	File file = new File("Data.txt");
 
 	@Override
 	public UserDTO getUser(int userId) throws DALException {
-		return null;
+		
+		UserDTO user = null;
+		
+		try{
+		FileInputStream f = new FileInputStream(file);
+		ObjectInputStream o = new ObjectInputStream(f);
+
+		
+		user = (UserDTO) o.readObject(); // ved ikke hvordan man finde den spesifike user... :(
+		
+		o.close();
+		f.close();
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		}catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return user;
 	}
 
 	@Override
@@ -16,6 +47,21 @@ public class PData implements IData {
 
 	@Override
 	public void createUser(UserDTO user) throws DALException {
+		
+		try {
+			FileOutputStream f = new FileOutputStream(file);
+			ObjectOutputStream o = new ObjectOutputStream(f);
+
+			o.writeObject(user);
+
+			o.close();
+			f.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		} 
 
 	}
 
