b48ba3a0a0f91f6a4c6936120916c94b2e969c71
absencia <absencia@inabsencia.clients.wireless.dtu.dk>
2017-02-20 15:38:45 +0100

    pull igen

--- CDIO-D1/src/logic/ILogic.java
+++ CDIO-D1/src/logic/ILogic.java
@@ -1,10 +1,11 @@
 package logic;
 
+import data.UserDTO;
 import data.IData.DALException;
 
 public interface ILogic {
-	void createUser(int userId, String cpr, String userName, String password, String ini, String role) throws DALException;
-	void updateUser(int userId, int i, String cpr) throws DALException;
+	void createUser(UserDTO user) throws DALException;
+	void updateUser(UserDTO user) throws DALException;
 	void deleteUser(int userId) throws DALException;
 	
 	boolean verifyPassword(String password);
