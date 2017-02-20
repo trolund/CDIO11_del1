a02b8d7534ed564e5b81149304ee2d5a4a424a76
absencia <absencia@inabsencia.clients.wireless.dtu.dk>
2017-02-20 13:00:33 +0100

    Small changes.

--- CDIO-D1/src/data/UserDTO.java
+++ CDIO-D1/src/data/UserDTO.java
@@ -93,7 +93,7 @@ public class UserDTO implements Serializable, Comparable<UserDTO> {
 
 	@Override
 	public String toString() {
-		return "Bruger Id: " + userId + ", Brugernavn: " + userName + ", Initialer: " + ini + ", Roller: " + roles;
+		return "Bruger id: " + userId + ", Brugernavn: " + userName + ", Initialer: " + ini + ", Roller: " + roles;
 	}
 
 }
